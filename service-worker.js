bc06e7efb0fbc2b72fa1d2ec02278dcd4d0d1231
adwqaz0965-a11y <adwqaz0965@gmail.com>
2025-12-27 18:28:08 +0700

    initial PWA app

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+const CACHE = "photo-finance-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
